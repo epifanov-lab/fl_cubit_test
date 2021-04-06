e22325249802efecd600e6b90500b5552dff4b21
Konstantin Epifanov <epifanov.lab@gmail.com>
2021-04-06 10:47:54 +0300

    rxdart added, bloc debounce check

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -4,6 +4,7 @@ import 'package:bloc/bloc.dart';
 import 'package:flcubittest/data/model/weather.dart';
 import 'package:flcubittest/data/weather_repository.dart';
 import 'package:meta/meta.dart';
+import 'package:rxdart/rxdart.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
@@ -12,6 +13,11 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   final WeatherRepository _weatherRepository;
 
   WeatherBloc(this._weatherRepository) : super(WeatherInitial());
+
+  @override
+  Stream<Transition<WeatherEvent, WeatherState>> transformEvents(Stream<WeatherEvent> events, transitionFn) {
+    return (events as Subject).debounceTime(Duration(milliseconds: 300));
+  }
 
   @override
   Stream<WeatherState> mapEventToState(
